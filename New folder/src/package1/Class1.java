3f5f08692c9d4f6f3629edf17f3049a2ceb90c23
rajesh <rajeshbommidi@gmail.com>
2014-12-18 23:19:05 +0530

    Committing changes 1

--- New folder/src/package1/Class1.java
+++ New folder/src/package1/Class1.java
@@ -5,7 +5,12 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hii hyd");
+		newFn();
 
+	}
+	
+	public static void newFn(){
+		System.out.println("new function");
 	}
 
 }
